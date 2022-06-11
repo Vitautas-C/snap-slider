f4d4bb7810e8787c61eff459aea89c0d7758bfc1
Vitautas-C <72389791+Vitautas-C@users.noreply.github.com>
2022-06-12 00:05:07 +0300

    add endless scroll

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const slider = document.querySelector(".slider")
+const preSlides = [...slider.children]
+const maxInertia = 30
+
+while (slider.children.length < maxInertia * 2 + 1) {
+  slider.append(...preSlides.map(slide => slide.cloneNode(true)))
+}
+
+updateSlidePlacement()
 
 onmouseup = () => {
   onmousemove = null
 
   snapClosestSlide()
 }
-
 
 ontouchend = () => {
   let timerId
@@ -32,6 +39,18 @@ slider.onmousedown = ({ clientX: x0 }) => {
 
 
 function snapClosestSlide() {
+  let timerId
+
+  slider.onscroll = () => {
+    clearTimeout(timerId)
+
+    timerId = setTimeout(() => {
+      slider.onscroll = null
+
+      updateSlidePlacement()
+    }, 50)
+  }
+
   slider.scrollTo({ left: getSlideWidth() * getCurrentSlideIndex(), behavior: "smooth" })
 }
 
@@ -43,4 +62,20 @@ function getSlideWidth() {
   const slideCount = slider.children.length
 
   return slider.scrollWidth / slideCount
+}
+
+function updateSlidePlacement() {
+  const i = getCurrentSlideIndex()
+  const leftSlidesCount = i
+  const rightSlidesCount = slider.children.length - i + 1
+  const leftSlidesRequired = maxInertia - leftSlidesCount
+  const rightSlidesRequired = maxInertia - rightSlidesCount
+
+  if (leftSlidesRequired > 0) {
+    slider.prepend(...[...slider.children].slice(-leftSlidesRequired))
+    slider.scrollLeft = (i + leftSlidesRequired) * getSlideWidth()
+  } else if (rightSlidesRequired > 0) {
+    slider.append(...[...slider.children].slice(0, rightSlidesRequired))
+    slider.scrollLeft = (i - rightSlidesRequired) * getSlideWidth()
+  }
 }
